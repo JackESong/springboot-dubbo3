a738f5840c7485f9ce61105161cd8fefc835a2a7
songjianwei <songjianwei@naton.cn>
2021-06-21 11:01:41 +0800

    feat:modify comsumer provider

---
+++ springboot-provider/src/main/java/com/example/demo/Dubbo3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dubbo3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dubbo3DemoApplication.class, args);
+    }
+
+}
---
+++ springboot-provider/src/main/java/com/example/demo/api/GreetingService.java
@@ -0,0 +1,47 @@
+package com.example.demo.api;
+
+/**
+ * <p>
+ *
+ * @CopyRright (c) NaTon
+ * <p>
+ * @Project: Dubbo3Demo
+ * @Comments: <p>
+ * @Package: com.example.dubbo3demo
+ * <p>
+ * @Author: naton
+ * <p>
+ * @Create Date:  2021/6/21 10:18 上午
+ * <p>
+ * @Modified By: naton
+ * <p>
+ * @Modified Date:  2021/6/21 10:18 上午
+ * <p>
+ * @Why & What is modified: <修改原因描述>
+ * <p>
+ * @Version: v1.0
+ */
+/*
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements.  See the NOTICE file distributed with
+ *   this work for additional information regarding copyright ownership.
+ *   The ASF licenses this file to You under the Apache License, Version 2.0
+ *   (the "License"); you may not use this file except in compliance with
+ *   the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+public interface GreetingService {
+
+    String sayHello(String name);
+
+}
